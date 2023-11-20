f0cc7c42189927e9565dfcd244e9161fcad6ab03
bisakita <bisakita@protonmail.com>
2023-11-20 15:02:40 +0000

    add Dockerfile and build push script

---
+++ build_push_image_order_service.sh
@@ -0,0 +1,31 @@
+#! /bin/sh
+
+# Membuat fungsi build image
+build_image(){
+        echo "=============== Build image ================"
+        docker build -t ghcr.io/bisakita/order-service:latest .
+        echo "\n\n"
+        sleep 1
+}
+
+# Membuat fungsi login github container registry
+login_github(){
+        echo "=============== Login Github ==============="
+        echo $PASS_GITHUB | docker login ghcr.io -u bisakita --password-stdin
+        echo "\n\n"
+        sleep 1
+}
+
+# Membuat fungsi login github container registry
+push_to_github(){
+        echo "============== Push to Github =============="
+        docker push ghcr.io/bisakita/order-service:latest
+        echo "================== done ===================="
+        echo "\n\n"
+        sleep 1
+}
+
+# Menjalankan fungsi berurutan
+build_image
+login_github
+push_to_github
